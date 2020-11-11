9ca980519e97164f37fa334b3bd5859aba1f3d13
André Sartori <afs25@cam.ac.uk>
2020-11-11 22:57:20 +0000

    #3705 comment: added extract_consent_statements.js

---
+++ dev/js/extract_consent_statements.js
@@ -0,0 +1,34 @@
+Qualtrics.SurveyEngine.addOnload(function()
+{
+    /*Place your JavaScript here to run when the page loads*/
+});
+
+Qualtrics.SurveyEngine.addOnReady(function()
+{
+    /*Place your JavaScript here to run when the page is fully displayed*/
+
+});
+
+Qualtrics.SurveyEngine.addOnPageSubmit(function()
+{
+    /*Place your JavaScript here to run when the page is submitted*/
+	var consent_dict = new Object();
+
+	let questions = Qualtrics.SurveyEngine.QuestionInfo;
+	var question_index;
+	for (question_index in questions) {
+		let question_data = new Qualtrics.SurveyEngine.QuestionData(question_index);
+		let question_info = questions[question_index];
+		let choices = question_info.Choices;
+		let answers = question_info.Answers;
+    	var choice_index;
+		for (choice_index in choices) {
+			choice_text = choices[choice_index].Text;
+			answer_code = question_data.getSelectedAnswerValue(choice_index);
+			answer_text = answers[answer_code].Display;
+			consent_dict[choice_text] = answer_text;
+		}
+	}
+
+	Qualtrics.SurveyEngine.setEmbeddedData('consent_statements', JSON.stringify(consent_dict));
+});
